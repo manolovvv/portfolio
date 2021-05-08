07b374d34e0e56f9044c8845a3696be5d44553e9
manolovvv <444652@student.fontys.nl>
2021-05-08 17:03:29 +0200

    Structure, Header, Home page added

---
+++ js/main.js
@@ -0,0 +1,46 @@
+// MENU SHOW
+
+const navMenu = document.getElementById("nav-menu"),
+toggleMenu = document.getElementById("nav-toggle"),
+closeMenu = document.getElementById("nav-close")
+
+//show
+toggleMenu.addEventListener('click',()=>{
+    navMenu.classList.toggle("show")
+})
+
+//hide
+closeMenu.addEventListener('click',()=>{
+    navMenu.classList.remove("show")
+})
+
+//remove menu
+const navLink = document.querySelectorAll(".nav__link")
+
+function linkAction(){
+    navMenu.classList.remove("show")
+}
+
+navLink.forEach(n => n.addEventListener("click",linkAction))
+
+
+//scroll sections active link
+const sections = document.querySelectorAll("section[id]")
+window.addEventListener('scroll',scrollActive)
+
+function scrollActive(){
+    const scrollY = window.pageXOffset
+    sections.forEach(current=>{
+        const sectionHeight = current.pageXOffsetHeight
+        const sectionTop = current.offsetTop - 50
+        sectionId = current.getAttribute('id')
+
+        if(scrollY>sectionTop && scrollY <= sectionTop +sectionHeight){
+            document.querySelector(".nav__menu a[href*=" +sectionId+"]").classList.add("actiove")
+
+        }
+        else{
+            document.querySelector(".nav__menu a[href*=" +sectionId+"]").classList.remove("actiove")
+        }
+    })
+}
