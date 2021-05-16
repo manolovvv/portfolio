565d54b532669425193a5af1077b6b6e61e7e3b2
Manolov <444652@student.fontys.nl>
2021-05-16 13:45:59 +0200

    sending email fixed

--- js/sendEmail.js
+++ js/sendEmail.js
@@ -1,5 +1,6 @@
 const form = document.querySelector('.contact__form')
 form.addEventListener('submit',(event)=>{
+  event.preventDefault()
     const data = new FormData(form)
     const subject = document.querySelector('#formSubject').value
     const email = document.querySelector('#formEmail').value
@@ -8,10 +9,11 @@ form.addEventListener('submit',(event)=>{
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
+      location.reload()
       console.log( this.responseText);
     }
   };
-  xhttp.open("POST", "http://localhost:2666/", true);
+  xhttp.open("POST", "https://send-email-manolovvv.herokuapp.com/", true);
   xhttp.setRequestHeader('Content-Type','application/json')
   xhttp.send(
       JSON.stringify(
